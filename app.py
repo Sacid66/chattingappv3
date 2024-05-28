add15b88afadeb185c17ca27128b21a9c3f89a95
Sacid66 <148653854+Sacid66@users.noreply.github.com>
2024-05-29 00:37:53 +0300

    güncellendi

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def on_join(data):
     color = random.choice(colors)
     join_room(room_id)
     rooms[room_id]['users'].append({'username': username, 'color': color})
-    send({'msg': f'{username} odaya katıldı.', 'username': 'Sistem', 'color': '#000000'}, room=room_id)
+    send({'msg': f'{username} has joined the room.', 'username': 'Sistem', 'color': '#000000'}, room=room_id)
     emit('user_color', {'username': username, 'color': color}, room=request.sid)
 
 @socketio.on('message')
@@ -113,7 +113,7 @@ def on_disconnect():
             if user['username'] == username:
                 rooms[room_id]['users'].remove(user)
                 break
-        send({'msg': f'{username} odadan ayrıldı.', 'username': 'System', 'color': '#000000'}, room=room_id)
+        send({'msg': f'{username} has left the room.', 'username': 'System', 'color': '#000000'}, room=room_id)
         if len(rooms[room_id]['users']) == 0:
             del rooms[room_id]
 
